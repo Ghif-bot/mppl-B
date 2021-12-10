3d52d5309fdcda2007377a9c053f935f678117da
rickys20 <73892020+rickys20@users.noreply.github.com>
2021-12-10 22:21:52 +0700

    update page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,7 +59,7 @@
                                                 id="exampleInputPassword" placeholder="Password">
                                         </div>
                                         
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                        <a href="/index" class="btn btn-primary btn-user btn-block">
                                             Login
                                         </a>
                                         <hr>
@@ -89,14 +89,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('admin/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{asset('admin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="{{asset('admin/js/sb-admin-2.min.js')}}"></script>
 
 </body>
 
